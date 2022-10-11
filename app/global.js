8c91704551521026455c924ecf5fd2ea7efb4e1d
Piet <80043493+Pietplus@users.noreply.github.com>
2022-10-11 15:20:25 +0200

    Update global.js
    rewriting this function

--- app/global.js
+++ app/global.js
@@ -1,12 +1,9 @@
-// When the user scrolls the page, execute myFunction
-window.onscroll = function() {myFunction()};
-
+// execute on scroll
+window.onscroll = () => myFunction();
 // Get the header
-var header = document.getElementById("Header");
-
+let header = document.getElementById("Header");
 // Get the offset position of the navbar
-var sticky = header.offsetTop;
-
+let sticky = header.offsetTop;
 // Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
 function myFunction() {
   if (window.pageYOffset > sticky) {
